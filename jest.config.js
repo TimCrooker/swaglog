6128eac5487e1a3bff5b680fae53e3c16ed132c8
Tim Crooker <timothycrooker@gmail.com>
2021-12-07 15:16:10 -0500

    update build

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const hq = require('alias-hq')
-
 module.exports = {
 	verbose: true,
 	testEnvironment: 'node',
 	modulePathIgnorePatterns: ['dist'],
-	moduleNameMapper: hq.get('jest'),
 }
---
+++ test/index.spec.ts
@@ -0,0 +1,5 @@
+describe('Logger', () => {
+	it('should log', () => {
+		expect(true).toBe(true)
+	})
+})
